5da5353097468d5c008ef583b3cc515ccf14a512
dylanbrandstetter <35245920+dylanbrandstetter@users.noreply.github.com>
2018-03-12 21:43:07 -0700

    Add database files

--- CST356 Week 4 Lab/Data/MyDbContext.cs
+++ CST356 Week 4 Lab/Data/MyDbContext.cs
@@ -9,7 +9,22 @@ namespace CST356_Week_4_Lab.Data
 {
     public class MyDbContext : DbContext
     {
+        public MyDbContext()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new MyDbInitializer());
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Pet> Pets { get; set; }
+    }
+
+    public class MyDbInitializer : DropCreateDatabaseIfModelChanges<MyDbContext>
+    {
+        // intentionally left blank
     }
 }
